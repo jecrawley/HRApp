e67ad3a186fffa22010a1fb34be1e641dbbd3f72
James Crawley <jamescrawley@James-ZCW-15.local>
2016-10-28 12:40:13 -0400

    Implemented user login. Cannot sign in unless user exists.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -42,4 +42,31 @@ angular.module('hrappApp')
               console.log(response);
           });
       }
+
+      $scope.login = function () {
+
+          var username = document.getElementById("username").value;
+          var pass = document.getElementById("pass").value;
+          var actualPass;
+
+          $http({
+              method:'GET',
+              url: 'http://localhost:8080/hrusers/search/findByUsername?username=' + username
+          }).
+          then( function (response) {
+
+              if (response.data._embedded.users[0] !== undefined) {
+                  actualPass = response.data._embedded.users[0].password
+              }
+
+              if (pass === actualPass) {
+                  userService.setUser(username);
+                  $location.path("/timecard");
+              } else {
+                  $scope.error = "Incorrect username or password!";
+              }
+
+          });
+
+      }
   });
